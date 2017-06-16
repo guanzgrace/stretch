6230458870038233bb870d78a9bf3d8768b6edee
guanzgrace <16graceg@gmail.com>
2017-06-16 00:23:59 -0500

    cleanup

--- notification.js
+++ notification.js
@@ -1,5 +1,8 @@
 // we rather not query the data into storage unless there is a huge need to.
 var isExerciseTooOld = false; 
+
+// check how long ago the exercises were saved, if they were saved more than
+// 1 month ago then re-get them and store them locally.
 chrome.storage.local.get('exercisesLastSaved', function(date) {
   var currentDate = new Date();
   var currentDate_ms = currentDate.getTime();
@@ -9,54 +12,44 @@ chrome.storage.local.get('exercisesLastSaved', function(date) {
     isExerciseTooOld = true;
   }
 });
-
 console.log(isExerciseTooOld);
 
+// if the exercise is too old, re-get from website and save to storage.
 if (isExerciseTooOld) { 
-    //  TO IMPLEMENT
-    // if exercise is too old, we need to re-get it from the website and save it to storage
     var exercises;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://physera.com/api/exercise", true);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        // innerText does not let the attacker inject HTML elements.
-        exercises = JSON.parse(xhr.responseText);
-        pickRandomExercise(exercises.results);
-        var d = new Date();
-        chrome.storage.local.set({'exercisesLastSaved': d.getTime()}, function() {
-              // Notify that we saved.
-              console.log("Current date " + d.getTime() + " saved as exercisesLastSaved.");
+        if (xhr.readyState == 4) {
+            // choose and display the exercise
+            exercises = JSON.parse(xhr.responseText);
+            pickRandomExercise(exercises.results);
+            // save the date we got this exercise
+            var d = new Date();
+            chrome.storage.local.set({'exercisesLastSaved': d.getTime()}, function() {
+                console.log("Current date " + d.getTime() + " saved as exercisesLastSaved.");
             });
 
-
-        results = exercises.results;
-        chrome.storage.local.set({'results': results}, function() {
-          console.log("Saved results.");
-          if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-            return;
-          }
-          chrome.storage.local.get('results', function(data) {
-            console.log(data);
-          });
-        });
-        
-        chrome.storage.local.set({'numExercises': results.length}, function() {
-            console.log("Num Exercises " + results.length + " saved as numExercises.");
-        });
-      }
-    }
-    xhr.send();
-
-    
-}
+            // save the results
+            results = exercises.results;
+            chrome.storage.local.set({'results': results}, function() {
+              console.log("Saved results.");
+              if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+                return;
+              }
+            });
+            
+            // save the number of exercises stored (currently not needed)
+            chrome.storage.local.set({'numExercises': results.length}, function() {
+                console.log("Num Exercises " + results.length + " saved as numExercises.");
+            });
+        } // end if readystate = 4 statement
+    } // end xhr on ready state change function
+    xhr.send();  
+} // end if exercise is too old statement
 else { // exercise is not too old
     // we just need to take exercise out of storage here.
-    pickRandomExerciseFromStorage();
-}
-
-function pickRandomExerciseFromStorage() {
     var results;
     chrome.storage.local.get('results', function(data) {
         results = data.results;
@@ -65,7 +58,7 @@ function pickRandomExerciseFromStorage() {
     });
 }
 
-
+// picks a random exercise, displays if it's valid
 function pickRandomExercise(exercises){
     var exerciseKeys = Object.keys(exercises);
     var randomKey = exerciseKeys[Math.floor(Math.random() * exerciseKeys.length)];
@@ -81,6 +74,7 @@ function pickRandomExercise(exercises){
     }
 }
 
+// javascript to append to the html page to display an exercise, precondition it is valid
 function displayExercise(selectedExercise) {
     var htmlText = '';
 
