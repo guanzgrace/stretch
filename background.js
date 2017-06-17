3e69f52c48eaa9167eea3f63cbe1647ee7665642
guanzgrace <16graceg@gmail.com>
2017-06-16 21:46:27 -0500

    options not fully functional

--- background.js
+++ background.js
@@ -1,16 +1,31 @@
 // opens the notifications tab every 30m/1h/2h
 // users should be able to set a start time hm
-var url = 'notification.html';
+
+/*var keys = ['freq', 'shv', 'smv', 'shav', 'ehv', 'emv', 'ehav'];
+var opts = [thirty, 9, 00, AM, 5, 00, PM]
+chrome.storage.local.get(keys, function(options) {
+    if (options.freq != null) opts[0] = options.freq;
+    if (options.shv != null) opts[0] = options.shv;
+    if (options.smv != null) opts[0] = options.smv;
+    if (options.shav != null) opts[0] = options.shav;
+    if (options.ehv != null) opts[0] = options.ehv;
+    if (options.emv != null) opts[0] = options.emv;
+    if (options.ehav != null) opts[0] = options.ehav;
+});
+
+console.log(opts);*/
+
+
 
 function createAlarm() {
     var now = new Date();
     var day = now.getDate();
-    if (now.getHours() >= 9) { // 9 AM already passed
+    if (now.getHours() >= 24) { // 9 AM already passed
         day += 1;
     }
-    
+
     // '+' casts the date to a number, like [object Date].getTime();
-    var timestamp = +new Date(now.getFullYear(), now.getMonth(), day, 9, 0, 0, 0);
+    var timestamp = +new Date(now.getFullYear(), now.getMonth(), day, 21, 7, 0, 0);
     //                        YYYY               MM              DD  HH MM SS MS
 
     // Create
@@ -21,15 +36,7 @@ function createAlarm() {
 }
 
 function openNotification() {
-    chrome.tabs.query({
-        url: url
-    }, function(tabs) {
-        if (tabs.length === 0) {
-            chrome.tabs.create({ url: url, active: true });
-        } else { // Focus first match, refresh the same page if it is already open
-            chrome.tabs.update(tabs[0].id, { active: true });
-        }
-    });
+    chrome.tabs.create({ url: 'notification.html', active: true });
 }
 
 // listen for time
---
+++ options.js
@@ -0,0 +1,44 @@
+document.getElementById("update").onclick = function() {
+	update();
+}
+
+function update() {
+	var frequency = document.getElementById("frequency");
+	var freqVal = frequency.options[frequency.selectedIndex].value;
+
+    var startHour = document.getElementById("startHour");
+    var startHourVal = startHour.options[startHour.selectedIndex].value;
+    var startMin = document.getElementById("startMin");
+    var startMinVal = startMin.options[startMin.selectedIndex].value;
+    var startHalf = document.getElementById("startHalf");
+    var startHalfVal = startHalf.options[startHalf.selectedIndex].value;
+
+    var endHour = document.getElementById("endHour");
+    var endHourVal = endHour.options[endHour.selectedIndex].value;
+    var endMin = document.getElementById("endMin");
+    var endMinVal = endMin.options[endMin.selectedIndex].value;
+    var endHalf = document.getElementById("endHalf");
+    var endHalfVal = endHalf.options[endHalf.selectedIndex].value;
+
+	chrome.storage.local.set({'freq': freqVal}, function() {
+      console.log("Saved frequency: " + freqVal);
+    });
+    chrome.storage.local.set({'shv': startHourVal}, function() {
+      console.log("Saved shv: " + startHourVal);
+    });
+    chrome.storage.local.set({'smv': startMinVal}, function() {
+      console.log("Saved smv: " + startMinVal);
+    });
+    chrome.storage.local.set({'shav': startHalfVal}, function() {
+      console.log("Saved shav: " + startHalfVal);
+    });
+    chrome.storage.local.set({'ehv': endHourVal}, function() {
+      console.log("Saved ehv: " + endHourVal);
+    });
+    chrome.storage.local.set({'emv': endMinVal}, function() {
+      console.log("Saved emv: " + endMinVal);
+    });
+    chrome.storage.local.set({'ehav': endHalfVal}, function() {
+      console.log("Saved ehav: " + endHalfVal);
+    });
+}
