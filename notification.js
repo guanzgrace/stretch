7420450f228ab641819028b4e1b28ff679d6a68c
guanzgrace <16graceg@gmail.com>
2017-06-16 20:28:44 -0500

    why were my changes deleted?

--- notification.js
+++ notification.js
@@ -1,17 +1,68 @@
-var exercises;
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://physera.com/api/exercise", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
-    exercises = JSON.parse(xhr.responseText);
-    pickRandomExercise(exercises.results);
-    //console.log(exercises);
+chrome.storage.local.getBytesInUse(null, function(bytes) {
+    console.log(bytes); // 5,242,880 bytes max
+});
+
+// we rather not query the data into storage unless there is a huge need to.
+var isExerciseTooOld = false; 
+
+// check how long ago the exercises were saved, if they were saved more than
+// 1 month ago then re-get them and store them locally.
+chrome.storage.local.get('exercisesLastSaved', function(date) {
+  var currentDate = new Date();
+  var currentDate_ms = currentDate.getTime();
+  console.log(currentDate_ms);
+  console.log(date);
+  if ((currentDate_ms - date) > (30 * 1000 * 60 * 60 * 24)) {
+    isExerciseTooOld = true;
   }
+});
+console.log(isExerciseTooOld);
+
+// if the exercise is too old, re-get from website and save to storage.
+if (isExerciseTooOld) { 
+    var exercises;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://physera.com/api/exercise", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            // choose and display the exercise
+            exercises = JSON.parse(xhr.responseText);
+            pickRandomExercise(exercises.results);
+            // save the date we got this exercise
+            var d = new Date();
+            chrome.storage.local.set({'exercisesLastSaved': d.getTime()}, function() {
+                console.log("Current date " + d.getTime() + " saved as exercisesLastSaved.");
+            });
+
+            // save the results
+            results = exercises.results;
+            chrome.storage.local.set({'results': results}, function() {
+              console.log("Saved results.");
+              if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+                return;
+              }
+            });
+            
+            // save the number of exercises stored (currently not needed)
+            chrome.storage.local.set({'numExercises': results.length}, function() {
+                console.log("Num Exercises " + results.length + " saved as numExercises.");
+            });
+        } // end if readystate = 4 statement
+    } // end xhr on ready state change function
+    xhr.send();  
+} // end if exercise is too old statement
+else { // exercise is not too old
+    // we just need to take exercise out of storage here.
+    var results;
+    chrome.storage.local.get('results', function(data) {
+        results = data.results;
+        console.log(results);
+        pickRandomExercise(results);
+    });
 }
-xhr.send();
 
-
+// picks a random exercise, displays if it's valid
 function pickRandomExercise(exercises){
     var exerciseKeys = Object.keys(exercises);
     var randomKey = exerciseKeys[Math.floor(Math.random() * exerciseKeys.length)];
@@ -27,6 +78,7 @@ function pickRandomExercise(exercises){
     }
 }
 
+// javascript to append to the html page to display an exercise, precondition it is valid
 function displayExercise(selectedExercise) {
     var htmlText = '';
 
