ccffd666718c242fc0b41ce89fd5109403ad5064
guanzgrace <16graceg@gmail.com>
2017-07-11 15:42:44 -0500

    remove minute

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ recreateAlarm();
 // creates the alarm with the given parameters: frequency, start hour value, start 
 // minute value, start half value, end hour value, end minute value, end half value
 // type to parseInt so that 2 + 3 =/= 23
-function createAlarm(freq, shv, smv, shav, ehv, emv, ehav) {
+function createAlarm(freq, shv, shav, ehv, ehav) {
     var now = new Date();
     var day = now.getDate();
 
@@ -21,7 +21,7 @@ function createAlarm(freq, shv, smv, shav, ehv, emv, ehav) {
     // Create the alarm, named alarmStart
     chrome.alarms.create('alarmStart', {
         when: timestamp,
-        periodInMinutes: 1 // for debugging, open the notifications every 1 min
+        periodInMinutes: freq // for debugging, open the notifications every 1 min
         //periodInMinutes: freq
     });
 }
@@ -56,8 +56,8 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
     var changedItems = Object.keys(changes);
  
     for (var item of changedItems) {
-        if (item == 'freq' || item == 'shv' || item == 'smv'
-            || item == 'shav' || item == 'ehv' || item == 'emv' || item == 'ehav') {
+        if (item == 'freq' || item == 'shv' || item == 'shav' 
+            || item == 'ehv' || item == 'ehav') {
             anythingImportantChanged = true;
         }
         console.log(item + " has changed:");
@@ -73,22 +73,18 @@ function recreateAlarm() {
     console.log("running recreateAlarm");
     var freq = 30; // default = 9-5, 30 min apart
     var shv = 9;
-    var smv = 0;
     var shav = 0;
     var ehv = 5;
-    var emv = 0;
     var ehav = 12;
 
     // query old options, null should be accounted for but just in case...
-    chrome.storage.local.get(['freq', 'shv', 'smv', 'shav', 'ehv', 'emv', 'ehav']
+    chrome.storage.local.get(['freq', 'shv', 'shav', 'ehv', 'ehav']
                                 , function(options) {
         if(options.freq != null) freq = parseInt(options.freq);
         if(options.shv != null)  shv = parseInt(options.shv);
-        if(options.smv != null)  smv = parseInt(options.smv);
         if(options.shav != null)  shav = parseInt(options.shav);
         if(options.ehv != null)  ehv = parseInt(options.ehv);
-        if(options.emv != null)  emv = parseInt(options.emv);
         if(options.ehav != null)  ehav = parseInt(options.ehav);
     });
-    createAlarm(freq, shv, smv, shav, ehv, emv, ehav);
+    createAlarm(freq, shv, shav, ehv, ehav);
 }
