18e460aa35c5a8ed0c02d9855a3a399905cab1f6
guanzgrace <16graceg@gmail.com>
2017-07-23 20:49:49 -0500

    updated backgroundjs comments

--- background.js
+++ background.js
@@ -1,31 +1,39 @@
 // upon launching, create the alarm
 recreateAlarm();
 
-// opens the notifications tab every 30m/1h/2h
-// creates the alarm with the given parameters: frequency, start hour value, start 
-// minute value, start half value, end hour value, end minute value, end half value
-// type to parseInt so that 2 + 3 =/= 23
+// opens the notifications tab every 30m/1h/2h at xx:30 and xx:00. xx is odd for every 2h.
 function createAlarm(freq) {
+    console.log("Calling createAlarm in background.js.")
     var now = new Date();
     var day = now.getDate();
-
-    // '+' casts the date to a number, like [object Date].getTime();
-    // YYYY               MM              DD     HH       MM  SS MS
+    
+    //                                     YYYY              MM  DD   HH MM SS MS
     var timestamp = +new Date(now.getFullYear(), now.getMonth(), day, 1, 0, 0, 0);
 
+    // clear the past alarmStart; then, create the alarm, named alarmStart.
     chrome.alarms.clearAll();
-    // Create the alarm, named alarmStart
     chrome.alarms.create('alarmStart', {
         when: timestamp,
         periodInMinutes: freq // for debugging, open the notifications every 1 min
-        //periodInMinutes: freq
+        //periodInMinutes: 1
     });
 }
 
 // opens the notification in a new browser tab.
 function openNotification() {
-    console.log("opening notification");
+    console.log("Calling openNotification in background.js.");
     chrome.tabs.create({ url: 'notification.html', active: true });
+}
+
+// recreates the alarm either by default or by storage, if they exist
+function recreateAlarm() {
+    console.log("Calling recreateAlarm in background.js.");
+    var freq = 30; // default 30 min apart
+    // query old options, account for null
+    chrome.storage.local.get('freq', function(options) {
+        if(options.freq != null) freq = parseInt(options.freq);
+    });
+    createAlarm(freq);
 }
 
 // listen for time and open the notification if it meets correct conditions
@@ -41,28 +49,13 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 
 // if storage is changed, recreate the alarm
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-    console.log("storage changed");
     var anythingImportantChanged = false;
     var changedItems = Object.keys(changes);
- 
     for (var item of changedItems) {
         if (item == 'freq') { anythingImportantChanged = true; }
-        console.log(item + " has changed:");
-        console.log("Old value: " + changes[item].oldValue);
-        console.log("New value: " + changes[item].newValue);
+        console.log(item + " has changed in storage: " + 
+            "Old value: " + changes[item].oldValue +
+            "New value: " + changes[item].newValue);
     }
-
     if (anythingImportantChanged) { recreateAlarm(); }
 });
-
-// recreates the alarm either by default or by storage, if they exist
-function recreateAlarm() {
-    console.log("running recreateAlarm");
-    var freq = 30; // default = 9-5, 30 min apart
-
-    // query old options, null should be accounted for but just in case...
-    chrome.storage.local.get('freq', function(options) {
-        if(options.freq != null) freq = parseInt(options.freq);
-    });
-    createAlarm(freq);
-}
